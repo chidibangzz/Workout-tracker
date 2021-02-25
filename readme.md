101f525808c0843fde5be8a2225b8f112eda6da3
Chidi Ekeke <chidiekeke18@yahoo.com>
2021-02-24 22:22:16 -0600

    added usercreateindex

--- readme.md
+++ readme.md
@@ -59,3 +59,31 @@ You are required to submit the following:
 * The URL to the deployed application
 
 * The URL to the GitHub repository
+
+Project title
+Git hub Come try my delicious burgers or build your own and add it to your menu, if you dont like the burger throw it in the trash.
+
+Motivation
+I wanted to incorporate all of the languages i learned by making a post request, styling with css and pulling information from mysql database to essentially interact with the user on my web page.
+
+Screenshots
+burgerFile
+
+Tech/framework used
+node Built with
+
+Electron
+How to use?
+When user loads web page they will be allowed to create a burger When user adds a burger it will be moved to the devoured column When user views devoured column the burger they added will be there When user hits "throw it away" it is deleted from devoured menu
+
+Contribute
+Please feel free contritbute to my project by contacting me A [contributing guideline](https://www.linkedin.com/in/chidi-ekeke-038609172/
+
+Credits
+Calvin Carter
+
+License
+Javascript, Html, Jquery, Node, mySql
+
+MIT © Chidi Ekeke
+
--- server.js
+++ server.js
@@ -3,50 +3,89 @@ const mongoose = require("mongoose");
 
 const db = require("./models/");
 
-const path = require("path")
+const path = require("path");
+const { exec } = require("child_process");
 
 const PORT = process.env.PORT || 3000
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useCreateIndex: true,
+      useFindAndModify: false
 });
+
 
 //routes
 app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname,"public/stats.html"))
+    res.sendFile(path.join(__dirname, "public/stats.html"))
 })
 app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname,"public/exercise.html"))
-    
-})   
+    res.sendFile(path.join(__dirname, "public/exercise.html"))
+
+})
 
 app.get("/api/workouts", (req, res) => {
-    db.Workout.find({}).then(function(data) {
-       res.json(data) 
+    db.Workout.aggregate([{
+        $addFields: {
+            totalDuration: {
+                $sum: "$exercises.duration"
+            }
+        }
+    }]).then(function (data) {
+        res.json(data)
     })
- });
-  
- app.post("/api/workouts", (req, res) => {
-    db.Workout.create({}).then(function(data) {
-       res.json(data) 
-    })
- });
+});
 
- app.post("/api/workouts/range", (req, res) => {
-    db.Workout.create({}).then(function(data) {
-       res.json(data) 
-    })
- });
- /api/workouts/range
 
-app.listen(PORT, ()=> {
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then(function (data) {
+        res.json(data)
+    })
+});
+
+app.post("/api/workouts/", (req, res) => {
+    db.Workout.create({}).then(function (data) {
+        res.json(data)
+    })
+});
+//     //chart route
+//  app.get("/api/workouts/range", (req, res) => {
+//     db.Workout.find({}).then(function(data) {
+//        res.json(data) 
+//     })
+//  });
+
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, {
+        $push: {
+            exercises: req.body
+        }
+    }).then(function (data) {
+        res.json(data)
+    })
+});
+
+
+
+app.get('/api/workouts/range', (req, res) => {
+    db.Workout.find({
+    }).sort({ _id: -1 })
+    .limit(7)
+    .then(function (data) {
+      res.json(data)
+    })
+});
+
+
+app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`)
 });
